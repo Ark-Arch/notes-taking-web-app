7c20c51c7ea7818f591b98b43a17087d6bbc3521
David Ifeoluwa Agbemuko <davidagbemuko.ad@gmail.com>
2024-03-13 11:21:47 +0100

    set up the sqlalchemy database on the flask application.

--- website/__init__.py
+++ website/__init__.py
@@ -1,11 +1,28 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+
+db = SQLAlchemy() # a database object created
+DB_NAME = "database.db"
 
 def create_app():
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'hkdalkkda kjagdlih'
     # the key above is responsible for encrypting the cookies/session data associated with the application.
 
-    from .views import views
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    # note that for the above line of code:
+    # it defines the appropriate URI of the database intended for use.
+    # URI -> Uniform Resource Identifier.
+    # it is the value of the URI that specifies the database connection details.
+    # notice the URI indicates that we are using SQLite as the database engine.
+    # and that database.db is the name of the SQLite database file.
+    # i could replace sqlite:/// with mysql:// or postgressql://
+
+
+    db.init_app(app) # this ties the database to this flask application.
+
+    # note that . indicates the current directory.
+    from .views import views 
     from .auth import auth
 
     app.register_blueprint(views, url_prefix='/')
---
+++ website/models.py
@@ -0,0 +1,6 @@
+from . import db
+from flask_login import UserMixin
+
+#importing from the current package (website folder) the db
+# could have been from website import db
+
